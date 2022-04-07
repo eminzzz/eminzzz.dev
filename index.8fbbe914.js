2657659c225e25215da752e13bfa39f36c89fb38
eminzzz <eminroth@gmail.com>
2022-04-07 19:52:13 +0300

    Updates

---
+++ index.8fbbe914.js
@@ -0,0 +1,2 @@
+!function(){const o=["#3b5998","#8a3ab9","#F8F8F2"];document.body.style.backgroundColor=(()=>{const n=Math.floor(Math.random()*o.length);return o[n]})()}();
+//# sourceMappingURL=index.8fbbe914.js.map
---
+++ index.bad0558f.js
@@ -0,0 +1,2 @@
+!function(){var o,a;a=["#3b5998","#8a3ab9","#F8F8F2"],document.body.style.backgroundColor=(o=Math.floor(Math.random()*a.length),a[o])}();
+//# sourceMappingURL=index.bad0558f.js.map
