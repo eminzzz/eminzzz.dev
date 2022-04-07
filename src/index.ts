801f79d7179239d31b1963cd7ca92e3b7f64d2e4
eminzzz <eminroth@gmail.com>
2022-04-07 19:26:06 +0300

    Keep project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+(function () {
+    const bgColors = [
+        "#3b5998",
+        "#8a3ab9",
+        "#F8F8F2"
+    ]
+    const randomBgColor = (): string => {
+        const randomIndex = Math.floor(Math.random() * bgColors.length)
+        return bgColors[randomIndex]
+    }
+
+    document.body.style.backgroundColor = randomBgColor()
+})();
