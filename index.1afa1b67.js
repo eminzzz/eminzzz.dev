c2cf97fc3676a67d906b8de2768b3e0820fbf766
eminzzz <eminroth@gmail.com>
2022-04-07 20:13:50 +0300

    Updates

---
+++ index.1afa1b67.js
@@ -0,0 +1,2 @@
+!function(){var n,o;n=["#3b5998","#8a3ab9","#F8F8F2"],o=function(){var o;document.body.style.backgroundColor=(o=Math.floor(Math.random()*n.length),n[o])},setInterval((function(){o()}),5e3),o()}();
+//# sourceMappingURL=index.1afa1b67.js.map
---
+++ index.b2826f34.js
@@ -0,0 +1,2 @@
+!function(){const o=["#3b5998","#8a3ab9","#F8F8F2"],t=()=>{document.body.style.backgroundColor=(()=>{const t=Math.floor(Math.random()*o.length);return o[t]})()};setInterval((()=>{t()}),5e3),t()}();
+//# sourceMappingURL=index.b2826f34.js.map
